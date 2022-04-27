225dcf9c2e48241653aa65db38741add09d3bfa3
Aashita Desai <aashita.desai@braze.com>
2022-04-27 14:52:41 -0400

    pagination fix

--- src/app/services/pokeapi.service.ts
+++ src/app/services/pokeapi.service.ts
@@ -1,95 +1,92 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { promise } from 'selenium-webdriver';
-import { abilities, pokemon_detail } from '../pokemon_card';
-import { element } from 'protractor';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { promise } from "selenium-webdriver";
+import { abilities, pokemon_detail } from "../pokemon_card";
+import { element } from "protractor";
 
 @Injectable()
 export class PokeapiService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  private pokeapi_list: string =
+    "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=";
 
-  private pokeapi_list:string = "https://pokeapi.co/api/v2/pokemon/?limit=20&amp;offset=";
-  
-  count:number = 0;
-  
+  count: number = 0;
+
   next() {
     this.count += 20;
   }
 
   previous() {
-    if(this.count >= 20) {
+    if (this.count >= 20) {
       this.count -= 20;
     }
   }
 
-  getPokeList():Promise<any> {
-    return this.http.get(this.pokeapi_list+this.count)
+  getPokeList(): Promise<any> {
+    return this.http
+      .get(this.pokeapi_list + this.count)
       .toPromise()
-      .then(data=>{
-         return data['results'] as any;
+      .then((data) => {
+        return data["results"] as any;
       })
-      .catch(err=>console.log(err));
-    }
-    
-    
-  getPokeImage(pokemon_url:string):Promise<any> {
-    return this.http.get(pokemon_url)
-    .toPromise()
-    .then(data => {
-      return data['sprites'] as any;
-    })
+      .catch((err) => console.log(err));
   }
 
-  getAbilityDetail(ability_url:string):Promise<abilities> {
-    let ability:abilities = new abilities();
-    
-    return this.http.get(ability_url)
-    .toPromise()
-    .then(data => {
-      ability.name = data['name'];
-      data['effect_entries'].map(element => {
-        ability.description = element['short_effect'];
+  getPokeImage(pokemon_url: string): Promise<any> {
+    return this.http
+      .get(pokemon_url)
+      .toPromise()
+      .then((data) => {
+        return data["sprites"] as any;
       });
-      return ability;
-    });
   }
 
-  getPokeDetail(pokemon_url:string):Promise<pokemon_detail> {
-    let pokemon:pokemon_detail= new pokemon_detail();
+  getAbilityDetail(ability_url: string): Promise<abilities> {
+    let ability: abilities = new abilities();
 
-    return this.http.get(pokemon_url)
-    .toPromise()
-    .then(data => {
-      //name
-      pokemon.name = data['name'];
-      
-      //image
-      this.getPokeImage(pokemon_url)
-      .then(res=>{
-        pokemon.front_default = res.front_default;
-        pokemon.back_default = res.back_default;
+    return this.http
+      .get(ability_url)
+      .toPromise()
+      .then((data) => {
+        ability.name = data["name"];
+        data["effect_entries"].map((element) => {
+          ability.description = element["short_effect"];
+        });
+        return ability;
       });
-
-      //weight
-      pokemon.weight = data['weight'];
-
-      pokemon.base_experience = data['base_experience'];
-      pokemon.height = data['height'];
-      
-      //abilities
-      pokemon.abilities = new Array<abilities>();
-      data['abilities'].map(element => {
-        this.getAbilityDetail(element['ability']['url'])
-        .then(a=>{
-          pokemon.abilities.push(a);
-        })
-      });
-      return pokemon;
-    });
   }
 
+  getPokeDetail(pokemon_url: string): Promise<pokemon_detail> {
+    let pokemon: pokemon_detail = new pokemon_detail();
+
+    return this.http
+      .get(pokemon_url)
+      .toPromise()
+      .then((data) => {
+        //name
+        pokemon.name = data["name"];
+
+        //image
+        this.getPokeImage(pokemon_url).then((res) => {
+          pokemon.front_default = res.front_default;
+          pokemon.back_default = res.back_default;
+        });
+
+        //weight
+        pokemon.weight = data["weight"];
+
+        pokemon.base_experience = data["base_experience"];
+        pokemon.height = data["height"];
+
+        //abilities
+        pokemon.abilities = new Array<abilities>();
+        data["abilities"].map((element) => {
+          this.getAbilityDetail(element["ability"]["url"]).then((a) => {
+            pokemon.abilities.push(a);
+          });
+        });
+        return pokemon;
+      });
+  }
 }
